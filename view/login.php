0f02e91e67d386a6ba3fddfce7c40711848354dd
Antoine Roulin <Antoine.ROULIN@cpnv.ch>
2021-03-08 11:12:11 +0100

    Ajout de front end login

---
+++ view/login.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @file      login.php
+ * @brief     This file is designed to display the login page login and error if necessary.
+ * @author    Created by Antoine Roulin
+ * @version   15.02.2021
+ */
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="/view/Login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="/view/Login/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="/view/Login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="/view/Login/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178" action="../index.php?action=login" method="post">
+					<span class="login100-form-title">
+						Sign In
+					</span>
+                    <?php if(isset($_GET["error"])) :?>
+                        <div class="alert alert-danger">
+                            <?php
+                                require "controller/error.php";
+                                echo checkErrors();
+                            ?>
+                        </div>
+                    <?php endif;?>
+                    <!-- username -->
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter email address">
+						<input class="input100" type="email" name="userInputEmail" placeholder="Email Address">
+						<span class="focus-input100"></span>
+					</div>
+
+                    <!-- password -->
+					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
+						<input class="input100" type="password" name="userPswd" placeholder="Password">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="text-right p-t-13 p-b-23">
+						<span class="txt1">
+							Forgot
+						</span>
+
+						<a href="#" class="txt2">
+							Username / Password?
+						</a>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Sign in
+						</button>
+					</div>
+
+					<div class="flex-col-c p-t-170 p-b-40">
+						<span class="txt1 p-b-9">
+							Don’t have an account?
+						</span>
+
+						<a href="#" class="txt3">
+							Sign up now
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/bootstrap/js/popper.js"></script>
+	<script src="/view/Login/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/daterangepicker/moment.min.js"></script>
+	<script src="/view/Login/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="/view/Login/js/main.js"></script>
+
+</body>
+</html>
