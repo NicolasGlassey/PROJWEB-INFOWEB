4574825b48b370ddbbc1b99ddfe2e4b96eba43b1
AntoineRoulinCPNV <61775794+AntoineRoulinCPNV@users.noreply.github.com>
2021-06-03 16:49:03 +0200

    Mise a jour de index.php et users.php

--- controller/users.php
+++ controller/users.php
@@ -36,6 +36,32 @@ function login($postDatas){
 }
 
 /**
+ * @brief This function is designed to redirect the user on the register page and check if this user is already registred, if not it checks the user credentials int the JSON users file
+ * @param $registerData : Datas coming from the register form , including ; email, firstname, lastname, username, password,phone number, locality, NPA and Street.
+ */
+function register($registerData){
+    if(isset($registerData["userInputEmail"]) && isset($registerData["userInputFirstname"]) && isset($$registerData["userInputLastname"]) && isset($registerData["userInputUsername"]) && isset($registerData["userInputPassword"]) && isset($registerData["userInputLocality"]) && isset($registerData["userInputNPA"]) && isset($registerData["userInputStreet"])){
+        try {
+            if(register($registerData)){ //Checking user credentials in the json file
+                $_SESSION["userName"] = $registerData["userInputUsername"];
+                require ("view/home.php");
+            }
+        }
+        catch (databaseException){
+            $error = 'An error has occured. Please try later';
+            require ("view/register.php");
+        }
+        catch (registeredException){
+            $error = 'You already have an account';
+            require ("view/register.php");
+        }
+    }
+    else{
+        require ("view/register.php");
+    }
+}
+
+/**
  * @brief This function is designed to log the user out of his session by destroying the $_SESSION variable
  */
 function logout(){
--- index.php
+++ index.php
@@ -18,6 +18,9 @@ if(isset($_GET['action'])){
         case "login":
             login($_POST);
             break;
+        case "register":
+            register($_POST);
+            break;
         case "logout":
             logout();
             break;
